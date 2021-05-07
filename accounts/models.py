4ec82ef33e19cf88a6211431b99bde4299487d53
hipema <a19hipema@iesgrancapitan.org>
2021-05-08 00:03:32 +0200

    actualización datos

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
+<<<<<<< Updated upstream
+=======
+# from stdimage import StdImageField, JPEGField
+>>>>>>> Stashed changes
 from django.conf import settings
 from django.dispatch import receiver 
 import os
