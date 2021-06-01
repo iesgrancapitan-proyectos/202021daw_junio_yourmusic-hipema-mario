9e7035b05309b2f297fe989c5d69ae7e5bce1d58
Mario <marioosuna26@gmail.com>
2021-06-01 08:10:04 +0200

    wip

--- noticias/views.py
+++ noticias/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Noticias
+from accounts.models import UserProfileMusicos,UserProfileOjeadores
 import datetime
 
 # Create your views here.
@@ -8,13 +10,31 @@ import datetime
 
 def index_noticias(request):
     """ __lte """
-    noticias = Noticias.objects.filter(created_at__lte=datetime.date.today())
-
-    # noticias = noticias.all()
+    noticias = Noticias.objects.filter(created_at__lte=datetime.date.today()).values()
+    '''
+    created_at, descripcion, id, title, updated_at, userprofilemusicos, userprofileojeadores
+    '''
+    print("NT")
+    usuarios=[]
+    for item in noticias:
+        
+        user=UserProfileMusicos.objects.filter(noticias=item['id']).values()
+        if user.count():
+            for i in user:                 
+                usuarios.append(i['nombre_profile'])
+            pass
+        else:
+            user=UserProfileOjeadores.objects.filter(noticias=item['id']).values()
+            for i in user:                 
+                usuarios.append(i['nombre_profile'])
+        
+    print(usuarios)
+            
+        
 
     paginator = Paginator(noticias, 4)
 
     page_number = request.GET.get('page')
     noticias_page = paginator.get_page(page_number)
-
-    return render(request, 'index_noticias.html', {'noticias':  noticias_page})
+    
+    return render(request, 'index_noticias.html', {'noticias':  noticias_page,'usuarios':usuarios})
